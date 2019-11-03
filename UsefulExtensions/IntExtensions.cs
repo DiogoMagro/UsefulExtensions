fa72931a367dd442e6a61c51041dc103abc491fb
Diogo Magro <38019726+JDiogoMagro@users.noreply.github.com>
2019-11-03 17:25:49 +0000

    Added summary to all extensions functions. Added new IntExtensions functions: -IsBetween. Added new StringExtensions functions: -ToInt; -GetBetween; -Trim: -ReplaceFrist; -ReplaceLast.

--- UsefulExtensions/IntExtensions.cs
+++ UsefulExtensions/IntExtensions.cs
@@ -9,44 +9,108 @@ namespace UsefulExtensions
     /// </summary>
     public static class IntExtensions
     {
+        /// <summary>
+        /// Indicates whether this instance is null.
+        /// </summary>
+        /// <param name="number">This int? instance.</param>
+        /// <returns></returns>
         public static bool IsNull(this int? number)
         {
             return number == null;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is null or zero.
+        /// </summary>
+        /// <param name="number">This int? instance.</param>
+        /// <returns></returns>
         public static bool IsNullOrZero(this int? number)
         {
             return number == null || number == 0;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is zero.
+        /// </summary>
+        /// <param name="number">This int? instance.</param>
+        /// <returns></returns>
         public static bool IsZero(this int? number)
         {
             return number == 0;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is zero.
+        /// </summary>
+        /// <param name="number">This int instance.</param>
+        /// <returns></returns>
         public static bool IsZero(this int number)
         {
             return number == 0;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is higher then zero.
+        /// </summary>
+        /// <param name="number">This int? instance.</param>
+        /// <returns></returns>
         public static bool IsPositive(this int? number)
         {
             return number > 0;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is higher then zero.
+        /// </summary>
+        /// <param name="number">This int instance.</param>
+        /// <returns></returns>
         public static bool IsPositive(this int number)
         {
             return number > 0;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is lower then zero.
+        /// </summary>
+        /// <param name="number">This int? instance.</param>
+        /// <returns></returns>
         public static bool IsNegative(this int? number)
         {
             return number < 0;
         }
 
+        /// <summary>
+        /// Indicates whether this instance is lower then zero.
+        /// </summary>
+        /// <param name="number">This int instance.</param>
+        /// <returns></returns>
         public static bool IsNegative(this int number)
         {
             return number < 0;
+        }
+
+        /// <summary>
+        /// Indicates if this instance is higher than the specified lowestNumber and lower than the specified highestNumber.
+        /// </summary>
+        /// <param name="number">This int instance.</param>
+        /// <param name="lowestNumber">The lowest number to compare.</param>
+        /// <param name="highestNumber">The highest number to compare.</param>
+        /// <returns></returns>
+        public static bool IsBetween(this int number, int lowestNumber, int highestNumber)
+        {
+            return number > lowestNumber && number < highestNumber;
+        }
+
+        /// <summary>
+        /// Indicates if this instance is higher than the specified lowestNumber and lower than the specified highestNumber.
+        /// </summary>
+        /// <param name="number">This int? instance.</param>
+        /// <param name="lowestNumber">The lowest number to compare.</param>
+        /// <param name="highestNumber">The highest number to compare.</param>
+        /// <returns></returns>
+        public static bool IsBetween(this int? number, int? lowestNumber, int? highestNumber)
+        {
+            return number > lowestNumber && number < highestNumber;
         }
     }
 }
--- UsefulExtensions/StringExtensions.cs
+++ UsefulExtensions/StringExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace UsefulExtensions
 {
@@ -8,28 +7,154 @@ namespace UsefulExtensions
     /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Indicates whether this instance is null or an empty string.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <returns></returns>
         public static bool IsNullOrEmpty(this string value)
         {
             return string.IsNullOrEmpty(value);
         }
 
+        /// <summary>
+        /// Indicates whether this instance is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <returns></returns>
         public static bool IsNullOrWhiteSpace(this string value)
         {
             return string.IsNullOrWhiteSpace(value);
         }
 
-        public static void ToUpperAtIndex (this string value, int i)
+        /// <summary>
+        /// Returns a copy of this string converted to uppercase at a specified index.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="index">The index to uppercase.</param>
+        /// <returns></returns>
+        public static string ToUpperAtIndex(this string value, int index)
         {
             char[] charArray = value.ToCharArray();
-            charArray[i] = Char.ToUpper(charArray[i]);
-            value = new string(charArray);
+            charArray[index] = Char.ToUpper(charArray[index]);
+            return new string(charArray);
         }
 
-        public static void ToLowerAtIndex(this string value, int i)
+        /// <summary>
+        /// Returns a copy of this string converted to lowercase at a specified index.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="index">The index to lowercase.</param>
+        /// <returns></returns>
+        public static string ToLowerAtIndex(this string value, int index)
         {
             char[] charArray = value.ToCharArray();
-            charArray[i] = Char.ToLower(charArray[i]);
-            value = new string(charArray);
+            charArray[index] = Char.ToLower(charArray[index]);
+            return new string(charArray);
+        }
+
+        /// <summary>
+        /// Convert this string to the equivalent integer.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="throwExceptionIfFailed">The bool value that indicates if should or not return exception in case of parse fails.</param>
+        /// <returns></returns>
+        public static int ToInt(this string value, bool throwExceptionIfFailed = false)
+        {
+            int result;
+            if (!int.TryParse(value, out result) && throwExceptionIfFailed)
+                throw new FormatException(string.Format("'{0}' cannot be converted as int", value));
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the string between the specified strStart and strEnd. 
+        /// Returns an empty string whether strStart or strEnd is not contained in the original string.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="strStart">The start string.</param>
+        /// <param name="strEnd">The end string.</param>
+        /// <returns></returns>
+        public static string GetBetween(this string value, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (value.Contains(strStart) && value.Contains(strEnd))
+            {
+                Start = value.IndexOf(strStart, 0) + strStart.Length;
+                End = value.IndexOf(strEnd, Start);
+                return value.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Returns the string between the specified indexStart and indexEnd.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="indexStart">The start index.</param>
+        /// <param name="indexEnd">The end index.</param>
+        /// <returns></returns>
+        public static string GetBetween(this string value, int indexStart, int indexEnd)
+        {
+            indexStart += 1;
+            return value.Substring(indexStart, indexEnd - indexStart);
+        }
+
+        /// <summary>
+        /// Removes all leading and trailing white-space characters from the current string object. 
+        /// In case the specified ignoreIfNull value be true this function will ignore null strings.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="allowNull">The bool value that indicates if null strings will be ignored.</param>
+        /// <returns></returns>
+        public static string Trim(this string value, bool ignoreIfNull)
+        {
+            if (ignoreIfNull)
+            {
+                if (value != null)
+                    return value.Trim();
+                else
+                    return value;
+            }
+            return value.Trim();
+        }
+
+        /// <summary>
+        /// Returns a new string in which the first occurrence of a specified 'search' string is replaced with a specified 'replace' string.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="search">The string to replace.</param>
+        /// <param name="replace">The string that will replace the specified 'search' string.</param>
+        /// <returns></returns>
+        public static string ReplaceFirst(this string value, string search, string replace)
+        {
+            int pos = value.IndexOf(search);
+
+            if (pos < 0)
+                return value;
+
+            return value.Substring(0, pos) + replace + value.Substring(pos + search.Length);
+        }
+
+
+        /// <summary>
+        /// Returns a new string in which the last occurrence of a specified 'search' string is replaced with a specified 'replace' string.
+        /// </summary>
+        /// <param name="value">This string instance.</param>
+        /// <param name="search">The string to replace.</param>
+        /// <param name="replace">The string that will replace the specified 'search' string.</param>
+        /// <returns></returns>
+        public static string ReplaceLast(this string value, string search, string replace)
+        {
+            int place = value.LastIndexOf(search);
+
+            if (place == -1)
+                return value;
+
+            return value.Remove(place, search.Length).Insert(place, replace);
         }
     }
 }
